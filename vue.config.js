d60a867327cd534f306fee7cff49f4d26ff0b60b
Alex-Briz <ua505444199@gmail.com>
2022-11-20 02:53:18 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/food-cart-vue/'
-  //   : '/'
-})
-
-module.exports = {
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/food-cart-vue/'
     : '/'
-}
+})
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/food-cart-vue/'
+//     : '/'
+// }
